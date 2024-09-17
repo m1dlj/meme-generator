f5c2240d09bb5edcff2dcf5c25ac358a86220e53
Your Name <your.email@example.com>
2024-09-17 22:09:13 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import trollFace from "../images/troll-face.png";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                alt="a meme face"
+                src={trollFace}  // Imported image
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    );
+}
